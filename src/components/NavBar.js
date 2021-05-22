b370b12af44bac6972dc2ea12edcd5efdfac7c27
Caseywalker <casey.walker118@gmail.com>
2021-05-22 14:04:30 -0500

    Adding routes

---
+++ src/components/NavBar.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+} from 'reactstrap';
+
+const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Casey Walker</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+        <Nav className="mr-auto" navbar>
+          <NavItem>
+            <Link className="nav-link" to="/">Home</Link>
+          </NavItem>
+          <NavItem>
+            <Link className="nav-link" to="/">Projects</Link>
+          </NavItem>
+        </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavBar;
